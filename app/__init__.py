c03340caa6ef6894bd54a015bc9edc68dce9f7bc
Folgert Karsdorp <fbkarsdorp@gmail.com>
2015-04-27 13:54:29 +0200

    add logging

--- app/__init__.py
+++ app/__init__.py
@@ -11,4 +11,14 @@ lm.session_protection = 'strong'
 lm.init_app(app)
 lm.login_view = 'login'
 
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/roodkapje.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('microblog startup')
+
 from app import views, models
