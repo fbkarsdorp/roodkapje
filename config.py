9bc7d5d113cfccf51203e3b88e6af82239e28a99
Folgert Karsdorp <fbkarsdorp@gmail.com>
2015-04-27 13:46:05 +0200

    add root path

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'roodkapje.db') +
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 ANNOTATION_DIR = os.path.join(basedir, "annotations")
+ROOT_DIR = basedir
