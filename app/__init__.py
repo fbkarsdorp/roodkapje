ff776cdf4cc42fdb467a79aaaa8faf9a017febcf
Folgert Karsdorp <fbkarsdorp@gmail.com>
2017-03-23 12:19:25 +0100

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -15,11 +15,13 @@ lm.login_view = 'login'
 if not app.debug:
     import logging
     from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/roodkapje.log', 'a', 1 * 1024 * 1024, 10)
+    if not os.path.isdir("tmp"):
+        os.mkdir("tmp")
+    file_handler = RotatingFileHandler('tmp/annotation.log', 'a', 1 * 1024 * 1024, 10)
     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     app.logger.setLevel(logging.INFO)
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
-    app.logger.info('roodkapje startup')
+    app.logger.info('annotation startup')
 
 from app import views, models
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # database
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'roodkapje.db') + '?check_same_thread=False'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'annotations.db') + '?check_same_thread=False'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 ANNOTATION_DIR = os.path.join(basedir, "annotations")
